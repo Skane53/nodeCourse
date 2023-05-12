ac83d31041bf63f5f1324a6cbd6136e1868dee9c
Samba Kane <sambakane175@gmail.com>
2023-05-12 13:37:12 +0000

    /public path express static

--- myApp.js
+++ myApp.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use(express.static("public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
